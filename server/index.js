8e2a9acfed2e9a849615fbe71dd37bed9dbd0af7
darshan-jpeg <darshanprasadjpeg@gmail.com>
2026-02-12 21:45:47 +0530

    index.js bug fix

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import express from 'express'
 import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import patientsRouter from './routes/patients.js'
 import doctorsRouter from './routes/doctors.js'
 import appointmentsRouter from './routes/appointments.js'
@@ -9,6 +12,7 @@ import appointmentsRouter from './routes/appointments.js'
 dotenv.config()
 
 const app = express()
+
 app.use(cors())
 app.use(express.json())
 
@@ -18,11 +22,22 @@ mongoose.connect(MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error', err))
 
+
 app.use('/api/patients', patientsRouter)
 app.use('/api/doctors', doctorsRouter)
 app.use('/api/appointments', appointmentsRouter)
 
-app.get('/', (req, res) => res.json({ ok: true }))
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(express.static(path.join(__dirname, '../dist')))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'))
+})
+
 
 const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`))
